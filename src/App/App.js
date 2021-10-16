46c95fabb38485a832dbec000c6b2706d5999859
JamesCrowleyTech <jamescrowley467@gmail.com>
2021-10-16 08:28:34 +0100

    Implemented node creation

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,45 @@
 import "./App.css";
+import { createContext, useReducer, useEffect } from "react";
+import reducer from "../reducer";
+
+const initialState = {
+    numberOfPeople: 300,
+    vaxRate: 20,
+    infectionChance: 50,
+};
+
+const mainContext = createContext(initialState);
 
 function App() {
-    return <div></div>;
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(function () {
+        const nodes = document.querySelectorAll(".node");
+        const vaxxedPopulation = Math.ceil(
+            (state.numberOfPeople * state.vaxRate) / 100
+        );
+        for (let i = 0; i < vaxxedPopulation; i++) {
+            nodes[i].classList.add("node--vaxxed");
+        }
+        if (vaxxedPopulation < state.numberOfPeople)
+            nodes[vaxxedPopulation].classList.add("node--infected");
+    });
+
+    return (
+        <mainContext.Provider value={{ state, dispatch }}>
+            <div className="app">
+                <div className="selection"></div>
+                {Array.apply(null, Array(state.numberOfPeople)).map(function (
+                    _,
+                    i
+                ) {
+                    return <div key={i} className="node"></div>;
+                })}
+            </div>
+        </mainContext.Provider>
+    );
 }
+
+export { mainContext };
 
 export default App;
